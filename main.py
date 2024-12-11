78c55b39edee9ebb493d36d01293c39a898e20a1
python-guy123 <vaishnav.sreekiran@gmail.com>
2024-12-11 20:33:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+x = True
+y = "Hello"
+z = 12
+if type(x) == bool:
+    print("Variable X is a boolean value.")
+else:
+    print("Variable X is not a boolean.")
+if type(y) == bool:
+    print("Variable Y is a boolean value.")
+else:
+    print("Variable Y is not a boolean.")
+if type(z) == bool:
+    print("Variable Z is a boolean value.")
+else:
+    print("Variable Z is not a boolean.")
